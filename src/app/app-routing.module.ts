fcfb750dbff853b1300abee43678de67dca724b1
Sidharth Sonar <Sidharthsonar1111@gmail.com>
2024-02-29 23:22:45 +0530

    minor nav settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,27 +6,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    // { // not needed for this project yet
-    //     path:"",
-    //     pathMatch:"full",
-    //     redirectTo:"/home",
-    // },
-    // {
-    //     path:"home",
-    //     component:HomeComponent,
-    // },
-    // {
-    //     path:"case-studies",
-    //     component:CaseStudiesComponent,
-    // },
-    // {
-    //     path:"about",
-    //     component:AboutComponent,
-    // },
-    // {
-    //     path:"catch-up",
-    //     component:CatchUpComponent,
-    // },
+    { // not needed for this project yet
+        path:"",
+        pathMatch:"full",
+        redirectTo:"/home",
+    },
+    {
+        path:"home",
+        component:HomeComponent,
+    },
+    {
+        path:"case-studies",
+        component:CaseStudiesComponent,
+    },
+    {
+        path:"about",
+        component:AboutComponent,
+    },
+    {
+        path:"catch-up",
+        component:CatchUpComponent,
+    },
     
 ];
 
